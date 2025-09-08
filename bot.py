dd5f36d77c5af8de98f24fad5068cbdb414c8a6b
Justin Schreiber <jschreiber234@gmail.com>
2025-09-08 19:33:05 -0400

    organized search and adder into class

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv
 import os
 import get_music_from_yt_link
+import search_and_add_to_playlist
 load_dotenv()
 
 url = os.getenv("url")# change to receive from message
 YOUTUBE_OPERATIONAL_API_URL=os.getenv("YOUTUBE_OPERATIONAL_API_URL")
+spotify_search_and_adder = search_and_add_to_playlist.SearchAndAdd()
 
 song_info = get_music_from_yt_link.find_music_info(url, YOUTUBE_OPERATIONAL_API_URL)
 
 if song_info != []:
-    print(song_info)
-
+    spotify_track_uri = spotify_search_and_adder.search_spotify_for_match(song_info)
+    spotify_search_and_adder.add_to_playlist([spotify_track_uri])
+    
     #else failed
 
--- search_and_add_to_playlist.py
+++ search_and_add_to_playlist.py
@@ -6,59 +6,60 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 #make into class so this can be instatiated in instance
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv("spotify_client_id"),
-                                               client_secret=os.getenv("spotify_client_secret"),
-                                               redirect_uri=os.getenv("redirect_uri"),
-                                               scope="playlist-modify-public, playlist-modify-private"))
+class SearchAndAdd:
 
-playlist_id = os.getenv('playlist_id')
-yt_music_info = [' アイドル','Yoasobi']
-spotify_track_uri = ''
+    def search_spotify_for_match(self, yt_music_info):
+        results = self.sp.search(q='track:'+yt_music_info[0]+' artist:'+yt_music_info[1],type='track')
+        spotify_music_info=[]
 
-def search_spotify_for_match(yt_music_info):
-    results = sp.search(q='track:'+yt_music_info[0]+' artist:'+yt_music_info[1],type='track')
-    spotify_music_info=[]
+        if len(results) > 0:
 
-    if len(results) > 0:
+            print('song match found')
+            spotify_music_info = [
+                results['tracks']['items'][0]['artists'][0]['name'],
+                results['tracks']['items'][0]['name'],
+                results['tracks']['items'][0]['album']['name'],
+                results['tracks']['items'][0]['external_urls']['spotify'],
+                results['tracks']['items'][0]['uri']
+            ]
 
-        print('song match found')
-        spotify_music_info = [
-            results['tracks']['items'][0]['artists'][0]['name'],
-            results['tracks']['items'][0]['name'],
-            results['tracks']['items'][0]['album']['name'],
-            results['tracks']['items'][0]['external_urls']['spotify'],
-            results['tracks']['items'][0]['uri']
-        ]
+            print('artist: '+spotify_music_info[0])
+            print('title: '+spotify_music_info[1])
+            print('album: '+spotify_music_info[2])
+            print('spotify url: '+spotify_music_info[3])
+            print('spotify uri: '+spotify_music_info[4])
+
+            return spotify_music_info[4]
+
+    def check_if_track_in_playlist(self, spotify_track_uri):
+        results = self.sp.playlist_items(self.playlist_id)
+        for item in results['items']:        
+            if item['track']['uri'] == spotify_track_uri:
+                return True
+        return False
+
+
+    def add_to_playlist(self, spotify_track_uri):
+        in_playlist = (self.check_if_track_in_playlist(spotify_track_uri))
+        if in_playlist:
+            print('the track is already part of the playlist. Skipping.....')
+        else:
+            self.sp.playlist_add_items(self.playlist_id,spotify_track_uri)
+            print('the song has been added to playlist')
+
+    def __init__(self):
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv("spotify_client_id"),
+                                                client_secret=os.getenv("spotify_client_secret"),
+                                                redirect_uri=os.getenv("redirect_uri"),
+                                                scope="playlist-modify-public, playlist-modify-private"))
+        self.playlist_id = os.getenv('playlist_id')
         
-        print('artist: '+spotify_music_info[0])
-        print('title: '+spotify_music_info[1])
-        print('album: '+spotify_music_info[2])
-        print('spotify url: '+spotify_music_info[3])
-        print('spotify uri: '+spotify_music_info[4])
-
-        return spotify_music_info[4]
-
-def check_if_track_in_playlist(spotify_track_uri):
-    results = sp.playlist_items(playlist_id)
-    for item in results['items']:        
-        if item['track']['uri'] == spotify_track_uri:
-            return True
-    return False
 
 
 
-spotify_track_uri = search_spotify_for_match(yt_music_info)
 
 
-in_playlist = (check_if_track_in_playlist(spotify_track_uri))
 
-
-def add_to_playlist(spotify_track_uri):
-    sp.playlist_add_items(playlist_id,spotify_track_uri)
-
-if not in_playlist:
-   add_to_playlist([spotify_track_uri])
-   print('the song has been added to playlist')
         
 
     
