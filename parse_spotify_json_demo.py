296440f823c26e3c1ca36c8f01baac5cdd51e97f
Justin Schreiber <jschreiber234@gmail.com>
2025-09-08 21:45:56 -0400

    removed demo used to parse spotify's json response

--- parse_spotify_json_demo.py
+++
@@ -1,9 +0,0 @@
-import json 
-
-with open('spotify_response_sample.info.json', encoding="utf8") as f:
-    spotify_json = json.load(f)
-    print(spotify_json['tracks']['items'][0]['artists'][0]['name'])
-    print(spotify_json['tracks']['items'][0]['name'])
-    print(spotify_json['tracks']['items'][0]['album']['name'])
-    print(spotify_json['tracks']['items'][0]['external_urls']['spotify'])
-    print(spotify_json['tracks']['items'][0]['uri'])
